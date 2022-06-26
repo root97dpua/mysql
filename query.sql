b7bafa98e2f3e9d466879e03b5b47eb85df4405d
user <email@email.com>
2022-06-26 21:50:30 +0300

    initial

--- query.sql
+++ query.sql
@@ -1,15 +1,17 @@
 drop table trains;
 
+
 create table trains
 (id int primary key auto_increment,
 number float not null unique,
 name varchar(30) not null
 );
 
+use railroad;
 
 insert into trains
 (number, name)
 values
-(rand(200), concat("train", rand(200)));
+(rand(200), concat('train', rand(200)));
 
-select * from trains;
+select number from trains;
